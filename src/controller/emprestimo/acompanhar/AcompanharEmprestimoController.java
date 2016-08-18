d897d620d48aa9d3ecfb267eeac3cab938a823c0
henriquehrf <henrique_rfirmino@hotmail.com>
2016-08-18 15:45:51 -0400

    Merge origin/master
    Conflicts:
    	src/controller/cadastro/Cadastro/CadastroPessoaController.java
    	src/utilitarios/i18N_pt_BR.properties

--- src/controller/emprestimo/acompanhar/AcompanharEmprestimoController.java
+++ src/controller/emprestimo/acompanhar/AcompanharEmprestimoController.java
@@ -7,7 +7,10 @@ package controller.emprestimo.acompanhar;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
@@ -16,12 +19,33 @@ import javafx.fxml.Initializable;
  */
 public class AcompanharEmprestimoController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Button btnVoltar;
+
+    @FXML
+    private Button btnVoltar1;
+
+    @FXML
+    private Button btnConsultar;
+
+    @FXML
+    void btnConsultarOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnVoltarOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnVoltar1OnAction(ActionEvent event) {
+
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
