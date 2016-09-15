62e8830f3d2a7295b5a63585a1acda51d4dee1c6
henriquehrf <henrique_rfirmino@hotmail.com>
2016-09-15 13:19:37 -0400

    Ajuste na tela de Login

---
+++ src/gui/LoginController.java
@@ -0,0 +1,145 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import classesAuxiliares.ClasseDoSistemaEstatico;
+import classesAuxiliares.NegociosEstaticos;
+import controller.PrincipalController;
+import static gui.SystemAutonet.SCENE;
+import java.awt.Event;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import vo.Pessoa;
+
+/**
+ * FXML Controller class
+ *
+ * @author Henrique
+ */
+public class LoginController {
+
+    @FXML
+    private Label labelUsuarioAlerta;
+
+    @FXML
+    private PasswordField txtSenha;
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Label labelSenhaAlerta;
+
+    @FXML
+    private Label lblautenticacao;
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private TextField txtUsuario;
+
+    private boolean autenticar() {
+        labelSenhaAlerta.setVisible(false);
+        labelUsuarioAlerta.setVisible(false);
+        lblautenticacao.setVisible(false);
+
+        Pessoa user = new Pessoa();
+        Pessoa otherUser = null;
+        user.setUsuario(txtUsuario.getText());
+        user.setSenha(txtSenha.getText());
+        try {
+            otherUser = NegociosEstaticos.getNegocioPessoa().buscarPorUsuario(user);
+            if (otherUser != null) {
+                if (otherUser.getSenha().equals(txtSenha.getText())) {
+
+                    ClasseDoSistemaEstatico.setPessoa(otherUser);
+                    return true;
+                } else {
+                    lblautenticacao.setText("*Senha Inválida");
+                    labelSenhaAlerta.setVisible(true);
+                    lblautenticacao.setVisible(true);
+                    return false;
+                }
+            }
+
+        } catch (Exception ex) {
+            lblautenticacao.setText("Usuário Inválido");
+            labelUsuarioAlerta.setVisible(true);
+            lblautenticacao.setVisible(true);
+            System.out.println("erro");
+            System.out.println(ex.getMessage());
+        }
+        return false;
+    }
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event){
+        if (autenticar()) {
+
+            BorderPane root = null;
+            try {
+                root = FXMLLoader.load(PrincipalController.class.getClassLoader().getResource("fxml/Principal.fxml"), ResourceBundle.getBundle("utilitarios/i18N_pt_BR"));
+            } catch (Exception ex) {
+                System.err.println(ex.getMessage());
+            }
+            Scene scene = new Scene(root);
+            SystemAutonet.SCENE = scene;
+            SystemAutonet.login.setScene(scene);
+            SystemAutonet.login.centerOnScreen();
+        }
+    }
+
+    @FXML
+    void btnCancelarOnAction(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    void txtUsuarioOnKeyPressed(KeyEvent event) {
+        habilitarLogin();
+    }
+
+    void habilitarLogin() {
+        if (!txtSenha.getText().isEmpty()) {
+            if (!txtUsuario.getText().isEmpty()) {
+                btnLogin.setDisable(false);
+                btnLogin.setDefaultButton(true);
+            }
+        } else {
+            btnLogin.setDisable(true);
+        }
+    }
+
+    @FXML
+    void txtSenhaonKeyPressed(KeyEvent event) {
+        habilitarLogin();
+    }
+
+    public void initialize() {
+        NegociosEstaticos.iniciar();
+        labelSenhaAlerta.setVisible(false);
+        labelUsuarioAlerta.setVisible(false);
+        btnLogin.setDisable(true);
+        lblautenticacao.setVisible(false);
+    }
+
+}
--- src/gui/SystemAutonet.java
+++ src/gui/SystemAutonet.java
@@ -43,84 +43,76 @@ import vo.Pessoa;
 public class SystemAutonet extends Application {
 
     public static Scene SCENE;
-
+    public static Stage login;
     @Override
     public void start(Stage stage) throws Exception {
-        NegociosEstaticos.iniciar();
-
-        Dialog<Pair<String, String>> dialog = new Dialog<>();
-        dialog.setTitle("Bem vindo ao SystemAutonet");
-        dialog.setHeaderText("Seja bem vindo ao sistema SystemAutonet");
-        //dialog.setContentText("Forneça um usuário e senha válido para realizar o acesso ao sistema");
-        // stage.getIcons().add(new Image("/utilitarios/icones/icone.png"));
-        ImageView image = new ImageView(this.getClass().getResource("/utilitarios/icones/networking.png").toString());
-        dialog.setGraphic(image);
-        ButtonType loginButtonType = new ButtonType("Login", ButtonData.OK_DONE);
-        dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
-
-        GridPane grid = new GridPane();
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(20, 150, 10, 10));
-
-        TextField username = new TextField();
-        username.setPromptText("Usuário");
-        PasswordField password = new PasswordField();
-        password.setPromptText("Senha");
-
-        grid.add(new Label("Usuário:"), 0, 0);
-        grid.add(username, 1, 0);
-        grid.add(new Label("Senha:"), 0, 1);
-        grid.add(password, 1, 1);
-        Node loginButton = dialog.getDialogPane().lookupButton(loginButtonType);
-        loginButton.setDisable(true);
-        username.textProperty().addListener((observable, oldValue, newValue) -> {
-            loginButton.setDisable(newValue.trim().isEmpty());
-        });
-
-        dialog.getDialogPane().setContent(grid);
-
-        Platform.runLater(() -> username.requestFocus());
-
-// Convert the result to a username-password-pair when the login button is clicked.
-        dialog.setResultConverter(dialogButton -> {
-            if (dialogButton == loginButtonType) {
-                return new Pair<>(username.getText(), password.getText());
-            }
-            System.exit(0);
-            return null;
-        });
-
-        Optional<Pair<String, String>> result = dialog.showAndWait();
-        boolean cond = true;
-        // Ajustar nesta parte do login
-        if (result.isPresent()) {
-            Pessoa user = new Pessoa();
-            Pessoa otherUser = null;
-            user.setUsuario(result.get().getKey());
-            user.setSenha(result.get().getValue());
-
-            otherUser = NegociosEstaticos.getNegocioPessoa().buscarPorUsuario(user);
-            if (otherUser != null) {
-
-                ClasseDoSistemaEstatico.setPessoa(otherUser);
-            } else {
-                System.out.println("erro");
-            }
-        }
+        
+        login=stage;
+//        Dialog<Pair<String, String>> dialog = new Dialog<>();
+//        dialog.setTitle("Bem vindo ao SystemAutonet");
+//        dialog.setHeaderText("Seja bem vindo ao sistema SystemAutonet");
+//        //dialog.setContentText("Forneça um usuário e senha válido para realizar o acesso ao sistema");
+//        // stage.getIcons().add(new Image("/utilitarios/icones/icone.png"));
+//        ImageView image = new ImageView(this.getClass().getResource("/utilitarios/icones/networking.png").toString());
+//        dialog.setGraphic(image);
+//        ButtonType loginButtonType = new ButtonType("Login", ButtonData.OK_DONE);
+//        dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+//
+//        GridPane grid = new GridPane();
+//        grid.setHgap(10);
+//        grid.setVgap(10);
+//        grid.setPadding(new Insets(20, 150, 10, 10));
+//
+//        TextField username = new TextField();
+//        username.setPromptText("Usuário");
+//        PasswordField password = new PasswordField();
+//        password.setPromptText("Senha");
+//
+//        grid.add(new Label("Usuário:"), 0, 0);
+//        grid.add(username, 1, 0);
+//        grid.add(new Label("Senha:"), 0, 1);
+//        grid.add(password, 1, 1);
+//        Node loginButton = dialog.getDialogPane().lookupButton(loginButtonType);
+//        loginButton.setDisable(true);
+//        username.textProperty().addListener((observable, oldValue, newValue) -> {
+//            loginButton.setDisable(newValue.trim().isEmpty());
+//        });
+//
+//        dialog.getDialogPane().setContent(grid);
+//
+//        Platform.runLater(() -> username.requestFocus());
+//
+//// Convert the result to a username-password-pair when the login button is clicked.
+//        dialog.setResultConverter(dialogButton -> {
+//            if (dialogButton == loginButtonType) {
+//                return new Pair<>(username.getText(), password.getText());
+//            }
+//            System.exit(0);
+//            return null;
+//        });
+//
+//        Optional<Pair<String, String>> result = dialog.showAndWait();
+//        boolean cond = true;
+//        // Ajustar nesta parte do login
+//        if (result.isPresent()) {
+            
+        
         Parent pane = null;
 
         try {
 
-            pane = FXMLLoader.load(PrincipalController.class.getClassLoader().getResource("fxml/Principal.fxml"), ResourceBundle.getBundle("utilitarios/i18N_pt_BR"));
+            pane = FXMLLoader.load(PrincipalController.class.getClassLoader().getResource("gui/Login.fxml"), ResourceBundle.getBundle("utilitarios/i18N_pt_BR"));
 
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
-        Scene scene = new Scene(pane, 800, 600);
-
+       // Scene login=new Scene(pane);
+      //  Scene scene = new Scene(pane);
+        Scene scene = new Scene(pane , stage.getWidth() , stage.getHeight());
+        
         stage.setResizable(false);
         SCENE = scene;
+     
 
         stage.setTitle("SystemAutonet - Sistema de Controle Simplificado");
         stage.getIcons().add(new Image("/utilitarios/icones/icone.png"));
@@ -133,6 +125,6 @@ public class SystemAutonet extends Application {
 
     public static void main(String[] args) {
 
-        launch(args);
+       Application.launch(SystemAutonet.class, args);
     }
 }
